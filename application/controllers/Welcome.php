4073f817762a329c4cb6933a95920fbcd1c4de89
Omegasad <tkahoso@my.bcit.ca>
2018-01-27 20:05:51 -0800

    Finished Basic Website to Good website convention tutorial

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,7 +19,26 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-		//$this->load->view('welcome');
+		// get the newest image from our model
+		$pix = $this->images->newest();
+		
+		// build an array of formatted cells for them
+		foreach ($pix as $picture)
+			$cells[] = $this->parser->parse('_cell', (array) $picture, true);
+			
+		// prime the table class
+		$this->load->library('table');
+		$params = array(
+			'table_open' => '<table class="gallery">',
+			'cell_start' => '<td class="oneimage">',
+			'cell_alt_start' => '<td class="oneimage">'
+		);
+		$this->table->set_template($params);
+		
+		//finally! generate the table
+		$rows = $this->table->make_columns($cells,3);
+		$this->data['thetable'] = $this->table->generate($rows);
+		
 		$this->data['pagebody'] = 'welcome';
 		$this->render();
 	}
